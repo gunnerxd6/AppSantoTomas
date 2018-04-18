ef38543dc6c7ca96f4b8ff6fc0b226bd00383db9
gunnerxd6 <victorortiz74@gmail.com>
2018-04-18 16:55:18 -0300

    Layouts

---
+++ app/src/main/java/com/example/victor/appsantotomas/ModificarGastosActivity.java
@@ -0,0 +1,13 @@
+package com.example.victor.appsantotomas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModificarGastosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_gastos);
+    }
+}
