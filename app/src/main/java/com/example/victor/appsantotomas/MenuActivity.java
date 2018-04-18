8af0a1c8111c7dc29f1372332a5473be3e03b9ba
gunnerxd6 <victorortiz74@gmail.com>
2018-04-18 16:54:29 -0300

    Layouts

--- app/src/main/java/com/example/victor/appsantotomas/MenuActivity.java
+++ app/src/main/java/com/example/victor/appsantotomas/MenuActivity.java
@@ -1,13 +1,33 @@
 package com.example.victor.appsantotomas;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.support.design.widget.FloatingActionButton;
+
 
 public class MenuActivity extends AppCompatActivity {
+    //declaracion de variables del tipo FloatingActionButton y ImageView
+    FloatingActionButton fab_agregargastos,fab_ingresos,fab_modificargastos,fab_estadisticas;
+    ImageView iv_salir,iv_configuracion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //Floatingactionbutton
+        fab_ingresos=findViewById(R.id.fab_ingresos);
+        fab_agregargastos=findViewById(R.id.fab_agregargastos);
+        fab_modificargastos=findViewById(R.id.fab_modificargastos);
+        fab_estadisticas=findViewById(R.id.fab_estadisticas);
+
+        //ImageView
+        iv_configuracion=findViewById(R.id.iv_configuracion);
+        iv_salir=findViewById(R.id.iv_salir);
+
     }
 }
