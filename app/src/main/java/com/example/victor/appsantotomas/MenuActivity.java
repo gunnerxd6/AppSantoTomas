53d13b702e8541d739b1ef92c05e4df7a51457c2
gunnerxd6 <victorortiz74@gmail.com>
2018-04-19 15:56:10 -0300

    Layouts 1.1

--- app/src/main/java/com/example/victor/appsantotomas/MenuActivity.java
+++ app/src/main/java/com/example/victor/appsantotomas/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.victor.appsantotomas;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +30,63 @@ public class MenuActivity extends AppCompatActivity {
         //ImageView
         iv_configuracion=findViewById(R.id.iv_configuracion);
         iv_salir=findViewById(R.id.iv_salir);
+
+
+        fab_ingresos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this,IngresosActivity.class);
+                startActivity(i);
+            }
+        });
+        fab_agregargastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this,AgregargastosActivity.class);
+                startActivity(i);
+            }
+        });
+        fab_modificargastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this,ModificarGastosActivity.class);
+                startActivity(i);
+            }
+        });
+        fab_estadisticas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this,EstadisticasActivity.class);
+                startActivity(i);
+            }
+        });
+        iv_configuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuActivity.this,ConfiguracionActivity.class);
+                startActivity(i);
+            }
+        });
+
+        iv_salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+                builder.setTitle("¿Salir?");
+                builder.setMessage("Salir a la pantalla inicial");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent o = new Intent(MenuActivity.this,MainActivity.class);
+                        startActivity(o);
+                        MenuActivity.this.finish();
+                    }
+                });
+                builder.setNegativeButton("Cancelar", null);
+                builder.show();
+
+            }
+        });
 
     }
 }
